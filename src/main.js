e9b26d476fc38e1fb966a5ead4a81d87b19ebf5c
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-21 16:39:28 -0700

    Add html and XMLhhttp Request for NASA API.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$('#submit').click(function() {
+
+  let request = new XMLHttpRequest();
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+
+  console.log(url);
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    $('.show').append(`<img src="${response.hdurl}">`);
+  }
+});
